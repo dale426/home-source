88baace8039c9d58e813b86f78b943f87ccfa402
10067142 <longyuli@juban.com>
2019-05-28 15:53:30 +0800

    'generate'

---
+++ assets/js/toolsData.js
@@ -0,0 +1,33 @@
+const categoryList = [{
+  title: '前端开发工具',
+  children: [
+    {
+      imgName: 'regexper.png',
+      desc: '正则表达式验证',
+      title: 'Regexper',
+      url: 'https://regexper.com/',
+      links: ['https://gitlab.com/javallone/regexper-static']
+    },
+    {
+      title: 'Linux命令大全',
+      imgName: 'linux.jpg',
+      desc: '在线搜索linux系统命令',
+      url: 'https://git.io/linux'
+    },
+    {
+      title: 'Tinypng',
+      desc: '在线png、jpg图片压缩',
+      imgName: 'tingpng.jpg',
+      url: 'https://tinypng.com/'
+    },
+    {
+      title: '520设计网',
+      desc: '在线配色工具、设计素材下载',
+      imgName: 'sj520.png',
+      url: 'http://www.sj520.cn/'
+    }
+  ]
+}]
+
+
+export default categoryList;
--- pages/tools/index.vue
+++ pages/tools/index.vue
@@ -1,39 +1,58 @@
 <template>
-  <div class="tools">
-    <h2 class="title">在线工具集</h2>
-    <div class="tools-main">
-      <ul>
-        <li v-for="(item, index) in menuList" :key="index">
-          <img :src="item.imgPath" alt="不支持显示">
-          <a href="">{{item.name}}</a>
-          <span>{{item.text}}</span>
-        </li>
-
-      </ul>
+    <div class="tools">
+        <div class="tools__main-title">在线工具集</div>
+        <div class="tools__main-items">
+            <!-- 分类工具 -->
+            <div class="tools__wrapper" v-for=" (item, index) in menuList" :key="index">
+                <div class="tools__title">前端开发</div>
+                <div class="tools__content">
+                    <div
+                        class="tools__item"
+                        v-for="(subItem, subIndex) in item.children"
+                        :key="subIndex"
+                    >
+                        <div class="tools__item-container">
+                            <div class="tools__item-logo">
+                                <a :href="subItem.url" target="_blank">
+                                    <img
+                                        :src="require(`@/assets/img/tool/${subItem.imgName}`)"
+                                        alt="不支持显示"
+                                    >
+                                </a>
+                            </div>
+                            <div class="tools__item-desc">
+                                <p class="tools__item-title">
+                                    <a :href="subItem.url" target="_blank">{{subItem.title}}</a>
+                                </p>
+                                <p class="tools__item-sub">{{subItem.desc}}</p>
+                                <p class="tools__item-links">
+                                    <a href="#">
+                                        <i class="iconfont iconcloud"></i>
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 <script>
+import categoryList from '@/assets/js/toolsData.js'
 export default {
-  name: 'tools',
-  data() {
-    return {
-      menuList: [{
-        imgPath: 'ss',
-        name: 'regexper',
-        text: '正则表达式在线验证'
-      }]
+    name: 'tools',
+    data() {
+        return {
+            menuList: []
+        }
+
+    },
+    created() {
+        this.menuList = categoryList;
     }
-  }  
 }
 </script>
 <style lang="less">
-.tools{
-  max-width: 960px;
-  margin: 0 auto;
-  color: #333;
-  .title{
-    text-align: center;
-  }
-}
+@import "./index.less";
 </style>
